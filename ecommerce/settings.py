818e44a364bc2ad8c3cb549a49eb26526617e50f
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-08-17 14:42:00 +0000

    updating ettings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for ecommerce project.
 
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +72,18 @@ TEMPLATES = [
         },
     },
 ]
+ 
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+   
+]
+
+
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
